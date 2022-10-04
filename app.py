c29f5a4443d701f7470647dd2c276c32a3296aef
lama1ftl <vsevolod.radionov@gmail.com>
2022-10-04 17:12:40 +0700

    initial commit

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import preprocessing, svm
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+
 
 
 df = pd.read_csv('bottle.csv')
